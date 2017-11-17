ce4f76338e8f2662db5578af80558816a756b4d8
lorcannolan <c15513717@mydit.ie>
2017-11-17 22:31:24 +0000

    End of day commit

--- app/src/main/java/dit/ie/foodstuff/AddNewItem.java
+++ app/src/main/java/dit/ie/foodstuff/AddNewItem.java
@@ -6,9 +6,14 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class AddNewItem extends Fragment
 {
+    private TextView name;
+    private TableLayout tableLayout;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
     {
@@ -19,6 +24,16 @@ public class AddNewItem extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.add_new_item, container, false);
+        View view = inflater.inflate(R.layout.add_new_item, container, false);
+
+        tableLayout = (TableLayout)container.findViewById(R.id.tableLayout);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView()
+    {
+        super.onDestroyView();
     }
 }
