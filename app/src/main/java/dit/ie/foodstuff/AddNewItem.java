3bc2c8e09ea655f60ed55a96b6a98536093a688d
lorcannolan <c15513717@mydit.ie>
2017-11-18 17:11:55 +0000

    Added text views and edit text to add_new_item xml

--- app/src/main/java/dit/ie/foodstuff/AddNewItem.java
+++ app/src/main/java/dit/ie/foodstuff/AddNewItem.java
@@ -6,14 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableLayout;
-import android.widget.TextView;
-
 public class AddNewItem extends Fragment
 {
-    private TextView name;
-    private TableLayout tableLayout;
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
     {
@@ -24,16 +18,7 @@ public class AddNewItem extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.add_new_item, container, false);
-
-        tableLayout = (TableLayout)container.findViewById(R.id.tableLayout);
-
-        return view;
+        return inflater.inflate(R.layout.add_new_item, container, false);
     }
 
-    @Override
-    public void onDestroyView()
-    {
-        super.onDestroyView();
-    }
 }
