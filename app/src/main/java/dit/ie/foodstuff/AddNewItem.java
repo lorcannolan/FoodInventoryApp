9327bec72407d3522437a9314e95512f901dd671
lorcannolan <c15513717@mydit.ie>
2017-11-24 00:20:21 +0000

    After scan, data is retrieved from open food facts api

--- app/src/main/java/dit/ie/foodstuff/AddNewItem.java
+++ app/src/main/java/dit/ie/foodstuff/AddNewItem.java
@@ -3,9 +3,13 @@ package dit.ie.foodstuff;
 import android.Manifest;
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -14,6 +18,7 @@ import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,8 +27,15 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
@@ -33,10 +45,16 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.CameraPreview;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class AddNewItem extends Fragment
 {
-    public EditText barcodeInput;
+    private static final String DEBUG_TAG = "HttpExample";
+    public EditText barcodeInput, nameInput;
     public String barcode;
+    private String url = "https://world.openfoodfacts.org/api/v0/product/";
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
@@ -148,6 +166,7 @@ public class AddNewItem extends Fragment
         }
 
         barcodeInput = (EditText)view.findViewById(R.id.enterBarcode);
+        nameInput = (EditText)view.findViewById(R.id.enterName);
 
         Button scan = (Button)view.findViewById(R.id.scan);
         scan.setOnClickListener(new View.OnClickListener()
@@ -184,8 +203,131 @@ public class AddNewItem extends Fragment
             }
             else
             {
-                barcodeInput.setText(result.getContents());
+                barcode = result.getContents();
+                barcodeInput.setText(barcode);
+                jsonConnect();
             }
         }
+    }
+
+    public void jsonConnect()
+    {
+        String stringUrl = url + barcode + ".json";
+        ConnectivityManager connMgr = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+        {
+            new DownloadWebpageTask().execute(stringUrl);
+        }
+        else
+        {
+            Toast.makeText(getActivity(), "Manually input details", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /* Use AsyncTask to create a task away from the main UI thread. This task takes a URL string and uses
+    it to create an HttpUrlConnection. Once the connection has been established, the AsyncTask downloads
+    the contents of the webpage as an InputStream. Finally, the InputStream is converted into a string,
+    which is displayed in the UI by the AsyncTask's onPostExecute method */
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String>
+    {
+        protected String doInBackground(String... urls) {
+            // params comes from the execute() call: params[0] is the url.
+            try
+            {
+                return downloadUrl(urls[0]);
+            }
+            catch (IOException e)
+            {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        protected void onPostExecute(String result)
+        {
+            String name = "";
+            try
+            {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONObject productInfo = (JSONObject)jsonObject.get("product");
+                name = productInfo.getString("product_name");
+            }
+            catch (JSONException e)
+            {
+                Log.e("MYAPP", "unexpected JSON exception", e);
+            }
+            nameInput.setText(name);
+        }
+    }
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+    // the web page content as a InputStream, which it returns as
+    // a string.
+
+    private String downloadUrl(String myurl) throws IOException
+    {
+        InputStream is = null;
+
+        try
+        {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is);
+
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        }
+        finally
+        {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+    // Reads an InputStream and converts it to a String.
+    // Reads an InputStream and converts it to a String.
+    private String readIt(InputStream is)
+    {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try
+        {
+            while ((line = reader.readLine()) != null)
+            {
+                sb.append(line).append('\n');
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                is.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
     }
 }
